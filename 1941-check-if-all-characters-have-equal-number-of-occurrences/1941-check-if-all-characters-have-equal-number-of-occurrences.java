efa684725b9bdb34c8dbdc016caaf257082878c4
V Gurumoorthy <gurumoorthy5306@gmail.com>
2026-07-23 09:36:39 +0530

    Time: 8 ms (37.2%), Space: 43.7 MB (15.05%) - LeetHub

---
+++ 1941-check-if-all-characters-have-equal-number-of-occurrences/1941-check-if-all-characters-have-equal-number-of-occurrences.java
@@ -0,0 +1,20 @@
+class Solution {
+    public boolean areOccurrencesEqual(String s)
+    {
+       HashMap<Character,Integer> map = new HashMap<>();
+       for(int i=0 ; i<s.length(); i++){
+           char c = s.charAt(i); 
+           map.put(c , map.getOrDefault(c,0)+1);
+    
+        }    
+        int value = map.get(s.charAt(0));
+        for(int v : map.values())
+        {
+            if(v != value)
+            {
+                return false;
+            }
+        }
+       return true;
+    }
+}  
